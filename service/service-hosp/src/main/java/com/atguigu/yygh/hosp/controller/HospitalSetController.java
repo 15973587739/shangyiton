ffcf8267587aec8b82de803a246c9a6e9cb12d3d
SIYU <2823005269@qq.com>
2023-08-30 10:41:19 +0800

    全局统一返回结果类 Result

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,5 +1,6 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.model.hosp.HospitalSet;
 //import com.atguigu.yygh.hosp.eitity.HospitalSet;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
@@ -8,8 +9,8 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.transform.Result;
 import java.util.List;
+import java.util.Random;
 
 @Api(tags = "医院设置管理")
 @RestController
@@ -22,48 +23,29 @@ public class HospitalSetController {
 
 
 
-    //1 查询医院设置表所有信息
+  //1 查询医院设置表所有信息
     @ApiOperation(value = "获取所有医院设置")
     @GetMapping("findAll")
-    public List<HospitalSet> findAllHospitalSet() {
+    public Result findAllHospitalSet() {
         //调用service的方法
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
     }
-
-
-//  //1 查询医院设置表所有信息
-//    @ApiOperation(value = "获取所有医院设置")
-//    @GetMapping("findAll")
-//    public Result findAllHospitalSet() {
-//        //调用service的方法
-//        List<HospitalSet> list = hospitalSetService.list();
-//        return Result.ok(list);
-//    }
 
 
 
     //2 逻辑删除医院设置
     @ApiOperation(value = "逻辑删除医院设置")
     @DeleteMapping("{id}")
-    public boolean removeHospSet(@PathVariable Long id) {
+    public Result removeHospSet(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
-        return flag;
+        if(flag) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
     }
-//
-//
-//    //2 逻辑删除医院设置
-//    @ApiOperation(value = "逻辑删除医院设置")
-//    @DeleteMapping("{id}")
-//    public Result removeHospSet(@PathVariable Long id) {
-//        boolean flag = hospitalSetService.removeById(id);
-//        if(flag) {
-//            return Result.ok();
-//        } else {
-//            return Result.fail();
-//        }
-//    }
-//
+
 //    //3 条件查询带分页
 //    @PostMapping("findPageHospSet/{current}/{limit}")
 //    public Result findPageHospSet(@PathVariable long current,
@@ -87,7 +69,7 @@ public class HospitalSetController {
 //        //返回结果
 //        return Result.ok(pageHospitalSet);
 //    }
-//
+
 //    //4 添加医院设置
 //    @PostMapping("saveHospitalSet")
 //    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet) {
@@ -104,29 +86,29 @@ public class HospitalSetController {
 //            return Result.fail();
 //        }
 //    }
-//
-//    //5 根据id获取医院设置
-//    @GetMapping("getHospSet/{id}")
-//    public Result getHospSet(@PathVariable Long id) {
-//        HospitalSet hospitalSet = hospitalSetService.getById(id);
-//        return Result.ok(hospitalSet);
-//    }
-//
-//    //6 修改医院设置
-//    @PostMapping("updateHospitalSet")
-//    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet) {
-//        boolean flag = hospitalSetService.updateById(hospitalSet);
-//        if(flag) {
-//            return Result.ok();
-//        } else {
-//            return Result.fail();
-//        }
-//    }
-//
-//    //7 批量删除医院设置
-//    @DeleteMapping("batchRemove")
-//    public Result batchRemoveHospitalSet(@RequestBody List<Long> idList) {
-//        hospitalSetService.removeByIds(idList);
-//        return Result.ok();
-//    }
+
+    //5 根据id获取医院设置
+    @GetMapping("getHospSet/{id}")
+    public Result getHospSet(@PathVariable Long id) {
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok(hospitalSet);
+    }
+
+    //6 修改医院设置
+    @PostMapping("updateHospitalSet")
+    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet) {
+        boolean flag = hospitalSetService.updateById(hospitalSet);
+        if(flag) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    //7 批量删除医院设置
+    @DeleteMapping("batchRemove")
+    public Result batchRemoveHospitalSet(@RequestBody List<Long> idList) {
+        hospitalSetService.removeByIds(idList);
+        return Result.ok();
+    }
 }
