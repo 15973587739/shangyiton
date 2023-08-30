e011addd3e630ccddb222f3e72ac0dee3fac92c0
冒得搞 <2381657699@qq.com>
2023-08-30 19:26:52 +0800

    完善model模块

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalApiController.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalApiController.java
@@ -1,8 +1,10 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.hosp.service.HospitalService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.ibatis.annotations.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/hosp/hospital")
 public class HospitalApiController {
-//    @Autowired
-//    private HospitalService hospitalService;
-//
+
+    @Autowired
+    private HospitalService hospitalService;
+
     @ApiOperation(value = "获取分页列表")
     @GetMapping("{page}/{limit}")
     public Result index(@PathVariable Integer page, @PathVariable Integer limit
-////            HospitalQueryVo hospitalQueryVo
+//            HospitalQueryVo hospitalQueryVo
     ) {
-//        //显示上线的医院
-//        //hospitalQueryVo.setStatus(1);
-////        Page<Hospital> pageModel = hospitalService.selectHospPage(page, limit, hospitalQueryVo);
-////        return Result.ok(pageModel);
+        //显示上线的医院
+        //hospitalQueryVo.setStatus(1);
+//        Page<Hospital> pageModel = hospitalService.selectHospPage(page, limit, hospitalQueryVo);
+//        return Result.ok(pageModel);
         return null;
 //
     }
