20a89fddc0a410b10c3a565a521731f30b8c3658
冒得搞 <2381657699@qq.com>
2023-08-30 11:52:06 +0800

    前端页面上传

---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalApiController.java
@@ -0,0 +1,30 @@
+package com.atguigu.yygh.hosp.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "医院管理接口")
+@RestController
+@RequestMapping("/api/hosp/hospital")
+public class HospitalApiController {
+//    @Autowired
+//    private HospitalService hospitalService;
+//
+    @ApiOperation(value = "获取分页列表")
+    @GetMapping("{page}/{limit}")
+    public Result index(@PathVariable Integer page, @PathVariable Integer limit
+////            HospitalQueryVo hospitalQueryVo
+    ) {
+//        //显示上线的医院
+//        //hospitalQueryVo.setStatus(1);
+////        Page<Hospital> pageModel = hospitalService.selectHospPage(page, limit, hospitalQueryVo);
+////        return Result.ok(pageModel);
+        return null;
+//
+    }
+}
---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/HospitalService.java
@@ -0,0 +1,8 @@
+package com.atguigu.yygh.hosp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HospitalService {
+
+}
