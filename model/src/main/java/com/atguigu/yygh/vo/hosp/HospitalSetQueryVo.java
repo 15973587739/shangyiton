662eb3de72090ba1e91ae9a221f24a2596d15277
冒得搞 <2381657699@qq.com>
2023-09-01 10:47:17 +0800

    model版本回退

---
+++ model/src/main/java/com/atguigu/yygh/vo/hosp/HospitalSetQueryVo.java
@@ -0,0 +1,17 @@
+package com.atguigu.yygh.vo.hosp;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * HospitalSetQueryVo
+ */
+@Data
+public class HospitalSetQueryVo {
+
+    @ApiModelProperty(value = "医院名称")
+    private String hosname;
+
+    @ApiModelProperty(value = "医院编号")
+    private String hoscode;
+}
