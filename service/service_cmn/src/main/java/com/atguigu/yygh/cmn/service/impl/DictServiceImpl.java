13a9b0331755262b31146891d6ec85a99e05c34b
冒得搞 <2381657699@qq.com>
2023-09-01 16:33:44 +0800

    添加数据字典开发（导出功能）

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/impl/DictServiceImpl.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/impl/DictServiceImpl.java
@@ -1,13 +1,20 @@
 package com.atguigu.yygh.cmn.service.impl;
 
 
+import com.alibaba.excel.EasyExcel;
 import com.atguigu.yygh.cmn.mapper.DictMapper;
 import com.atguigu.yygh.cmn.service.DictService;
 import com.atguigu.yygh.model.cmn.Dict;
+import com.atguigu.yygh.vo.cmn.DictEeVo;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,6 +33,33 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
             dict.setHasChildren(isChild);
         }
         return dictList;
+    }
+
+    @Override
+    public void exportDictData(HttpServletResponse response) {
+
+            //设置类型
+            response.setContentType("application/vnd.ms-excel");
+            //设置编码
+            response.setCharacterEncoding("utf-8");
+            String fileName = "dict";
+            response.setHeader("Content-disposition","attachment;filename="+fileName+".xlsx");
+            //查询数据库
+            List<Dict> dictList = baseMapper.selectList(null);
+            //因为写入的时候引入的是vo类所以需要将Dict转换成DictEeVo
+            List<DictEeVo> dictEeVoList =new ArrayList<>();
+            for(Dict dict : dictList){
+                DictEeVo dictEeVo = new DictEeVo();
+                BeanUtils.copyProperties(dict,dictEeVo);
+                dictEeVoList.add(dictEeVo);
+            }
+        try {
+            //调用方法进行写操作
+            EasyExcel.write(response.getOutputStream(), DictEeVo.class).sheet("dict")
+                    .doWrite(dictEeVoList);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     //判断id下是否有子数据
