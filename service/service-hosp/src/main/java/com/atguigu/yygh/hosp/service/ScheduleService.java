1d8bd7e133c5644798271fdca599f6cb29ebba8a
SIYU <2823005269@qq.com>
2023-09-02 09:41:00 +0800

    四、查询科室接口 1、添加service接口 selectPage 2、添加controller接口  department 五、删除科室接口 1、添加service接口 remove 2、添加controller接口  removeDepartment 六、上传排班接口 1.1 添加model Schedule 1.2 添加repository  ScheduleRepository 1.3 添加service接口及实现类  ScheduleService 2、上传排班 2.1 接口数据分析 2.2 添加service接口 save 2.3 添加repository接口 getScheduleByHoscodeAndHosScheduleId 2.4 添加controller接口  saveSchedule 七、查询排班接口 1、添加service接口 selectPage 2、添加controller接口 schedule 八、删除排班接口 1、添加service接口 remove 2、添加controller接口 removeSchedule

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/ScheduleService.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/ScheduleService.java
@@ -1,14 +1,14 @@
 package com.atguigu.yygh.hosp.service;
 
-import com.atguigu.yygh.model.hosp.Department;
-import com.atguigu.yygh.vo.hosp.DepartmentQueryVo;
+import com.atguigu.yygh.model.hosp.Schedule;
+import com.atguigu.yygh.vo.hosp.ScheduleQueryVo;
 import org.springframework.data.domain.Page;
 
 import java.util.Map;
 
-public interface DepartmentService {
+public interface ScheduleService {
     /**
-     * 上传科室信息
+     * 上传排班信息
      * @param paramMap
      */
     void save(Map<String, Object> paramMap);
@@ -17,17 +17,17 @@ public interface DepartmentService {
      * 分页查询
      * @param page 当前页码
      * @param limit 每页记录数
-     * @param departmentQueryVo 查询条件
+     * @param scheduleQueryVo 查询条件
      * @return
      */
-    Page<Department> selectPage(Integer page, Integer limit, DepartmentQueryVo departmentQueryVo);
+    Page<Schedule> selectPage(Integer page, Integer limit, ScheduleQueryVo scheduleQueryVo);
 
     /**
      * 删除科室
      * @param hoscode
-     * @param depcode
+     * @param hosScheduleId
      */
-    void remove(String hoscode, String depcode);
+    void remove(String hoscode, String hosScheduleId);
 
 
 }
