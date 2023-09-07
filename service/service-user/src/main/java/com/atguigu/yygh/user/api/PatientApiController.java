f0d6a9a2a2217e55ebc05a0d90fe9dae140aca26
SIYU <2823005269@qq.com>
2023-09-07 20:29:38 +0800

    2、搭建service-order模块 2.1 搭建service-order模块 搭建过程参考service-user模块 2.2 修改配置 2、添加配置文件application.properties 2.3 启动类 2.4配置网关 3、添加订单基础类 3.1 添加model 3.2 添加Mapper 3.3 添加service接口及实现类 3.4 添加controller 4.2 搭建service-user-client模块 4.2.1 搭建service-user-client模块

---
+++ service/service-user/src/main/java/com/atguigu/yygh/user/api/PatientApiController.java
@@ -0,0 +1,18 @@
+package com.atguigu.yygh.user.api;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Api(tags = "就诊人管理接口")
+@RestController
+@RequestMapping("/api/user/patient")
+public class PatientApiController {
+
+//    @Autowired
+//    private PatientService patientService;
+
+}
